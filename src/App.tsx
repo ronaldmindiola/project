96137eb8d0dc9a96fec422da0d67b37a25672b44
Ronald Mindiola <ronaldmindiola@live.com>
2024-06-07 15:42:34 -0500

    Logos, Animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import tailwindLogo from './assets/tailwind.svg'
+import shadcnLogo from './assets/shadcn.svg'
+import reactRouterLogo from './assets/react-router.svg'
+
 import { Button } from './components/atoms/Button'
 
 
 function App() {
   const [count, setCount] = useState(0)
+  const [animateOut, setAnimateOut] = useState(false);
+  const [animateIn, setAnimateIn] = useState(false);
+  useEffect(() => {
+    if (animateOut) {
+      const timer = setTimeout(() => {
+        setAnimateOut(false);
+        setAnimateIn(true);
+        const timerIn = setTimeout(() => {
+          setAnimateIn(false);
+        }, 300); // 300ms = 0.3 segundos, la duración de la animación de entrada
 
+        return () => clearTimeout(timerIn);
+      }, 300); // 300ms = 0.3 segundos, la duración de la animación de salida
+
+      return () => clearTimeout(timer);
+    }
+  }, [animateOut]);
+
+  const handleButtonClick = () => {
+    setAnimateOut(true);
+    setTimeout(() => {
+      setCount(count + 1);
+    }, 300); // Cambia el estado del contador después de la animación de salida
+  };
   return (
     <>
     <div className='flex flex-col h-screen items-center justify-center'>
@@ -22,9 +48,20 @@ function App() {
         <a href="https://tailwindcss.com/" target="_blank">
           <img src={tailwindLogo} className="p-4 h-20" alt="Tailwind CSS logo" />
         </a>
+        <a href="https://ui.shadcn.com/" target="_blank">
+          <img src={shadcnLogo} className="p-4 h-20" alt="Tailwind CSS logo" />
+        </a>
+        <a href="https://reactrouter.com/en/main" target="_blank">
+          <img src={reactRouterLogo} className="p-4 h-20" alt="React Router logo" />
+        </a>
         
       </div>
-      <Button variant="default"  onClick={() => setCount((count) => count + 1)}>Contador: {count}</Button>
+      <div className="m-1">
+      <Button variant="default" onClick={handleButtonClick}>
+          Contador: 
+          <span className={`ml-2 font-black ${animateOut ? 'animate-ping-out' : ''} ${animateIn ? 'animate-ping-in' : ''}`}>{count}</span>
+        </Button></div>
+      
       <div className="m-8">
         
         <p className='mt-8'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -66,10 +66,39 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        ping: {
+          '75%, 100%': {
+            transform: 'scale(2)',
+            opacity: '0',
+          },
+        },
+        "ping-in": {
+          '0%': {
+            transform: 'translateY(50%)',
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'translateY(0)',
+            opacity: '1',
+          },
+        },
+        "ping-out": {
+          '0%': {
+            transform: 'translateY(0)',
+            opacity: '1',
+          },
+          '100%': {
+            transform: 'translateY(-50%)',
+            opacity: '0',
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        ping: "ping 0.3s cubic-bezier(0, 0, 0.2, 1) 1",
+        "ping-in": "ping-in 0.5s cubic-bezier(0.25, 0.8, 0.25, 1) 1",
+        "ping-out": "ping-out 0.5s cubic-bezier(0.25, 0.8, 0.25, 1) 1",
       },
     },
   },
